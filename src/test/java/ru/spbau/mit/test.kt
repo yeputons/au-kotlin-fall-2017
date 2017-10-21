22a3d872f90fac180fc5737063404eb662f85d08
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-10-21 15:02:15 +0300

    Add sample project and README

---
+++ src/test/java/ru/spbau/mit/test.kt
@@ -0,0 +1,10 @@
+package ru.spbau.mit
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TestSource {
+    @Test
+    fun testGreeting() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
