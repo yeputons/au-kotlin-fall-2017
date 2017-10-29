1bce461b57cd7bb281b593b375103ae80fa35b16
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-10-29 16:38:26 +0300

    Add antlr example

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,5 +1,10 @@
 package ru.spbau.mit
 
+import org.antlr.v4.runtime.BufferedTokenStream
+import org.antlr.v4.runtime.CharStreams
+import ru.spbau.mit.parser.ExpLexer
+import ru.spbau.mit.parser.ExpParser
+
 fun getGreeting(): String {
     val words = mutableListOf<String>()
     words.add("Hello,")
@@ -10,5 +15,6 @@ fun getGreeting(): String {
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val expLexer = ExpLexer(CharStreams.fromString("(1 + 2)"))
+    println(ExpParser(BufferedTokenStream(expLexer)).eval().value)
 }
