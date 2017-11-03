1fb1cb401a8a1e060e33e20b8841159f5a684500
Egor Suvorov <egor.suvorov@gmail.com>
2017-11-03 16:57:33 +0300

    Add testMaxTest (TLs locally on my machine)

--- src/test/java/ru/spbau/mit/testMain.kt
+++ src/test/java/ru/spbau/mit/testMain.kt
@@ -2,6 +2,7 @@ package ru.spbau.mit
 
 import org.junit.Test
 import java.io.ByteArrayOutputStream
+import java.util.*
 import kotlin.test.assertEquals
 
 class MainTest {
@@ -24,5 +25,25 @@ class MainTest {
         val outputStream = ByteArrayOutputStream()
         solve(input.byteInputStream(), outputStream)
         assertEquals(expected, outputStream.toString().replace("\r\n", "\n"))
+    }
+
+    @Test
+    fun testMaxTest() {
+        val height = 3
+        val width = 100000
+        val queries = 100000
+        val rnd = Random()
+        val flagLines = (0 until height).joinToString("\n") {
+            (0 until width).joinToString(" ") { _ -> Integer.toString(rnd.nextInt(1000000) + 1) }
+        }
+        val queryLines = (0 until queries).joinToString("\n") {
+            val a = rnd.nextInt(width) + 1
+            val b = rnd.nextInt(width) + 1
+            val l = minOf(a, b)
+            val r = maxOf(a, b)
+            "$l $r"
+        }
+        val data = "$height $width $queries\n$flagLines\n$queryLines\n"
+        solve(data.byteInputStream(), ByteArrayOutputStream())
     }
 }
