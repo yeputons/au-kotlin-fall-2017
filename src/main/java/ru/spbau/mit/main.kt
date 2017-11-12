a9c6a300ad2efbd256b1533e168fd82588823903
Egor Suvorov <egor.suvorov@gmail.com>
2017-11-12 23:57:56 +0300

    Add main.kt; remove test.kt

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,25 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import org.antlr.v4.runtime.BufferedTokenStream
+import org.antlr.v4.runtime.CharStreams
+import ru.spbau.mit.parser.LangLexer
+import ru.spbau.mit.parser.LangParser
+import java.nio.file.Paths
 
-    return words.joinToString(separator = " ")
+fun interpreterPrintln(args: List<InterpreterValue>): InterpreterValue? {
+    println(args.joinToString(separator = " ") { it.toString() })
+    return 0
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    if (args.size != 1) {
+        System.err.println("Usage: main <file-to-interpret>")
+        System.exit(1)
+    }
+    val filename = Paths.get(args[0])
+    val program = LangParser(BufferedTokenStream(LangLexer(CharStreams.fromPath(filename)))).file().value!!
+    val context = BaseInterpretationContext(createStdlibScope(
+            println = { interpreterPrintln(it) }
+    ))
+    program.statements.forEach({ context.run(it) })
 }
