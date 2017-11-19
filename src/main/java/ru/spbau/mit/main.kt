ae286ae2c50ecf6c3ca5e6316d64d1fde515e4f7
Egor Suvorov <egor.suvorov@gmail.com>
2017-11-19 20:40:34 +0300

    Extend example in main.kt

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -2,6 +2,7 @@ package ru.spbau.mit
 
 import ru.spbau.mit.tex.curlyArguments
 import ru.spbau.mit.tex.document
+import ru.spbau.mit.tex.plainArgument
 import ru.spbau.mit.tex.squareArguments
 
 fun main(args: Array<String>) {
@@ -44,6 +45,28 @@ fun main(args: Array<String>) {
                 |
                 """.trimMargin())
             }
+        }
+
+        frame("This is just a text frame") {
+            // Copied from https://en.wikibooks.org/wiki/LaTeX/Presentations#References_.28Beamer.29
+            +"As the reference list grows, the reference slide will divide into two slides and so on, through use of the allowframebreaks option."
+            +"Individual items can be cited after adding an 'optional' label to the relevant bibitem stanza."
+            +"The citation call is simply \\cite. Beamer also supports limited customization of the way references are presented (see the manual)."
+            +"Those who wish to use natbib, for example, with Beamer may need to troubleshoot both their document setup and the relevant BibTeX style file."
+            newParagraph()
+            +"The different types of referenced work are indicated with a little symbol (e.g. a book, an article, etc.)."
+            +"The Symbol is set with the commands "; textbf {+"beamertemplatebookbibitems"}; +" and beamertemplatearticlebibitems."
+            +"It is also possible to use setbeamertemplate directly, like so"
+        }
+
+        frame {
+            customCommand("includegraphics",
+                    squareArguments(
+                            "width" to """\textwidth""",
+                            "height" to """0.8\textheight""",
+                            plainArgument("keepaspectratio")
+                    ),
+                    curlyArguments("mypicture.png"))
         }
     }.toOutputStream(System.out)
 }
