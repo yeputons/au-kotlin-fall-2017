b92c2248ae8d950f1d7160071b9468a428cc8f4d
Egor Suvorov <egor.suvorov@gmail.com>
2017-11-30 18:05:25 +0300

    Don't run the program if there are syntax errors

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,11 +1,13 @@
 package ru.spbau.mit
 
+import org.antlr.v4.runtime.ANTLRErrorListener
 import org.antlr.v4.runtime.BufferedTokenStream
 import org.antlr.v4.runtime.CharStreams
 import ru.spbau.mit.ast.ast
 import ru.spbau.mit.parser.LangLexer
 import ru.spbau.mit.parser.LangParser
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 fun interpreterPrintln(args: List<InterpreterValue>): InterpreterValue? {
     println(args.joinToString(separator = " ") { it.toString() })
@@ -18,7 +20,13 @@ fun main(args: Array<String>) {
         System.exit(1)
     }
     val filename = Paths.get(args[0])
-    val program = LangParser(BufferedTokenStream(LangLexer(CharStreams.fromPath(filename)))).file().ast()
+    val parser = LangParser(BufferedTokenStream(LangLexer(CharStreams.fromPath(filename))))
+    val antlrAst = parser.file()
+    if (parser.numberOfSyntaxErrors > 0) {
+        System.err.println("Syntax errors detected, won't run")
+        exitProcess(1)
+    }
+    val program = antlrAst.ast()
     val context = BaseInterpretationContext(createStdlibScope(
             println = { interpreterPrintln(it) }
     ))
